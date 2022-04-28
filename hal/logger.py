fe4599a5dbb35a7d2ea97e1a44506d3f5b44868f
Atharv Joshi <atharvjoshi@ymail.com>
2022-04-28 17:09:12 +0800

    Add logger to log Hal's activity

---
+++ hal/logger.py
@@ -0,0 +1,22 @@
+""" log hal's activity """
+
+from pathlib import Path
+
+from loguru import logger
+
+# set logs folder path
+LOGSPATH = Path.cwd() / "logs"
+
+logger.remove()  # remove default handlers
+
+# register log sinks with loguru logger
+logger.add(
+    LOGSPATH / "log_{time}.log",
+    format="<cyan>[{time:YY-MM-DD HH:mm:ss}]</> <lvl>[{module}] - {message}</>",
+    rotation="24 hours",  # current log file closed and new one started every 24 hours
+    retention="1 month",  # log files created more than a month ago will be removed
+    backtrace=True,
+    diagnose=True,
+)
+
+logger.debug("Logger activated!")

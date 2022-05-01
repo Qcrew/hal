f4fd4705737595d56ee9a33c59d49270257c7382
Atharv Joshi <atharvjoshi@ymail.com>
2022-05-01 17:20:51 +0800

    Prepare config.py for taking over LOGSPEC and PARSESPEC

---
+++ hal/config.py
@@ -0,0 +1,3 @@
+""" Module that handles configuration settings for reading logged data and posting data on Notion page. These settings include (1) what parameters are to be logged, (2) which logfiles are they located in, (3) which position in the logfile they are located in, (4) how will their raw values be parsed before displaying on Notion, and (5) which position block in the Notion page will they be displayed on. This config file is the only file a user would have to edit if they wish to add/remove parameters and/or give them a different meaning/arrangement. """
+
+

fdfec93bee730f422724b06e350165c821919f88
Atharv Joshi <atharvjoshi@ymail.com>
2023-01-17 00:15:51 +0800

    Handle reading of params by column or keyword position hints

--- hal/reader.py
+++ hal/reader.py
@@ -4,20 +4,17 @@ from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 
+from hal.config import LOGFOLDER, PARAMS
 from hal.param import Param
 
 
 class Reader:
     """ """
 
-    def __init__(self, path: str, *params: Param) -> None:
-        """
-        path (str) path to the main logs folder
-        params (Param) a sequence of Params to be read from their log file
-        """
-        self.path: Path = Path(path)
-        self.params: tuple[Param] = params
-        self.delimiter: str = ","
+    def __init__(self) -> None:
+        """ """
+        self._path: Path = Path(LOGFOLDER)
+        self._params: tuple[Param] = PARAMS
         self._data: dict[Param, dict[str, str]] = self._read(last=False)
 
     @property
@@ -26,10 +23,10 @@ class Reader:
         # get current date in yy-mm-dd format e.g. 23-01-12
         date = datetime.now().strftime("%y-%m-%d")
         # get dict with key = Param and value = logfile Path
-        fpaths = {p: self.path / f"{date}/{p.filename}{date}.log" for p in self.params}
+        paths = {p: self._path / f"{date}/{p.filename}{date}.log" for p in self._params}
         # return dict with key = logfile Path and value = list[Param]
         logspec = defaultdict(list)
-        for param, filepath in fpaths.items():
+        for param, filepath in paths.items():
             logspec[filepath].append(param)
         return logspec
 
@@ -38,18 +35,24 @@ class Reader:
         Internal method to read logfiles. If last=True, data dictionary value contains only the last timestamp and value pair, if last=False, we read last 'param.nvals' in reverse chronological order for each Param.
         return dict with same structure as read() and make same assumptions as read()
         """
-        data = {param: {} for param in self.params}
+        data = {param: {} for param in self._params}
         for path, params in self.logspec.items():
             if path.exists():  # empty data dict if path does not exist
                 with path.open() as file:
                     tokens = [line.rstrip("\n").split(",") for line in file.readlines()]
-                keys = [param.key for param in params if param.key]
                 for param in params:  # read 'nvals' or latest token(s) for each param
                     nvals = 1 if last else param.nvals
                     for token in tokens[-nvals:][::-1]:
-                        if all(key in token for key in keys):  # ignore bad tokens
-                            timestamp = f"{token[0]} {token[1]}"
+                        timestamp = f"{token[0]} {token[1]}"
+                        if isinstance(param.pos, int):  # pos = col index
                             data[param][timestamp] = token[param.pos]
+                        elif isinstance(param.pos, str):  # pos = keyword adjacent
+                            try:
+                                idx = token.index(param.pos)
+                            except ValueError:  # keyword not present in token = bad log
+                                pass  # ignore bad log
+                            else:  # assume value is right next to position keyword
+                                data[param][timestamp] = token[idx + 1]
         return data
 
     def read(self) -> dict[Param, dict[str, str]]:
